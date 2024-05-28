5aba593d1387648ad54140f79d9b3cce07aa6500
ZofearZ <64070203@it.kmitl.ac.th>
2024-05-28 14:23:20 +0700

    upsecur-ping

--- regions/us-west-2/securitygroups.tf
+++ regions/us-west-2/securitygroups.tf
@@ -119,6 +119,13 @@ resource "aws_security_group" "ec2_security_group_vpc_2" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "ICMP - ping"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
